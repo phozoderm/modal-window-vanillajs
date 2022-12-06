fde54c3a004f14c424dd8f8a92043500d185c6f7
Safiye Türk <phzdrmm@gmail.com>
2022-12-07 00:23:48 +0300

    Final commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+// console.log(btnsOpenModal)
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = function (){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+};
+const openModal = function (){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+for (let i = 0; i < btnsOpenModal.length; i++)
+   btnsOpenModal[i].addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e){
+    // console.log(e.key)
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+})
+
